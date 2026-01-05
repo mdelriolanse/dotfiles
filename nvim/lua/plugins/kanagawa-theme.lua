458c5f582adb5f3ede088f995bead75fd839db3d
Mateo del Rio <mdelriolanse@gmail.com>
2026-01-05 10:00:53 -0500

    Clangd and pylsp support

--- nvim/lua/plugins/kanagawa-theme.lua
+++ nvim/lua/plugins/kanagawa-theme.lua
@@ -16,14 +16,61 @@ return {
         palette = {},
         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
       },
-      overrides = function(colors) -- add/modify highlights
-        return {}
+      overrides = function(colors)
+        local theme = colors.theme
+        local palette = colors.palette
+
+        return {
+          -- Dark completion menu background
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+
+          -- nvim-cmp highlights
+          CmpItemAbbr = { fg = theme.ui.fg },
+          CmpItemAbbrDeprecated = { fg = theme.syn.comment, strikethrough = true },
+          CmpItemAbbrMatch = { fg = palette.dragonBlue, bold = true },
+          CmpItemAbbrMatchFuzzy = { fg = palette.dragonBlue, bold = true },
+          CmpItemMenu = { fg = theme.syn.comment, italic = true },
+
+          -- Completion item kinds
+          CmpItemKindDefault = { fg = theme.ui.fg_dim },
+          CmpItemKindText = { fg = theme.ui.fg },
+          CmpItemKindMethod = { fg = palette.dragonBlue },
+          CmpItemKindFunction = { fg = palette.dragonBlue },
+          CmpItemKindConstructor = { fg = palette.dragonYellow },
+          CmpItemKindField = { fg = palette.dragonGreen2 },
+          CmpItemKindVariable = { fg = palette.dragonWhite },
+          CmpItemKindClass = { fg = palette.dragonYellow },
+          CmpItemKindInterface = { fg = palette.dragonYellow },
+          CmpItemKindModule = { fg = palette.dragonBlue },
+          CmpItemKindProperty = { fg = palette.dragonGreen2 },
+          CmpItemKindUnit = { fg = palette.dragonOrange },
+          CmpItemKindValue = { fg = palette.dragonOrange },
+          CmpItemKindEnum = { fg = palette.dragonYellow },
+          CmpItemKindKeyword = { fg = palette.dragonPink },
+          CmpItemKindSnippet = { fg = palette.dragonTeal },
+          CmpItemKindColor = { fg = palette.dragonRed },
+          CmpItemKindFile = { fg = theme.ui.fg },
+          CmpItemKindReference = { fg = palette.dragonRed },
+          CmpItemKindFolder = { fg = palette.dragonBlue },
+          CmpItemKindEnumMember = { fg = palette.dragonGreen2 },
+          CmpItemKindConstant = { fg = palette.dragonOrange },
+          CmpItemKindStruct = { fg = palette.dragonYellow },
+          CmpItemKindEvent = { fg = palette.dragonYellow },
+          CmpItemKindOperator = { fg = palette.dragonRed },
+          CmpItemKindTypeParameter = { fg = palette.dragonGreen2 },
+        }
       end,
       theme = 'dragon', -- Load "wave" theme
       background = { -- map the value of 'background' option to a theme
-        dark = 'wave', -- try "dragon" !
+        dark = 'dragon',
         light = 'lotus',
       },
     }
+
+    -- Apply the colorscheme
+    vim.cmd.colorscheme 'kanagawa'
   end,
 }
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,5 @@
+-- Additional LSP setups that aren't handled by mason-lspconfig
+-- pylsp is temporarily disabled - reinstall via :MasonInstall python-lsp-server
+-- then uncomment the pylsp setup below
+return {}
+
